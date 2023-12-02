e4285b10ffbdda1a4a49e71e41074bf16b6b0d6e
JedisBrownie <Natana.ralambomanana@gmail.com>
2023-12-02 15:17:59 +0300

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -56,16 +56,16 @@ const App: React.FC = () => (
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonIcon aria-hidden="true" icon={ellipse} />
+            <IonLabel>General</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
             <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonLabel>Domicile</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonIcon aria-hidden="true" icon={ellipse} />
+            <IonLabel>Exterieur</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,22 +1,61 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCol, IonGrid, IonRow } from '@ionic/react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://soccer.bsite.net/soccer-stats-controller/general/1');
+        console.log(response.data);
+        setData(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Statistiques generales</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <IonGrid>
+          <IonRow>
+            <IonCol>Equipe</IonCol>
+            <IonCol>Competition</IonCol>
+            <IonCol>Buts</IonCol>
+            <IonCol>Tirs pm</IonCol>
+            <IonCol>Discipline</IonCol>
+            <IonCol>Possession %</IonCol>
+            <IonCol>Passes réussies %</IonCol>
+            <IonCol>Aeriens gagnes</IonCol>
+            <IonCol>Note</IonCol>
+          </IonRow>
+          
+          {/* Use map to iterate through the data and create rows */}
+          {data.map((item, index) => (
+            <IonRow key={index}>
+              <IonCol>{item._equipeNom}</IonCol>
+              <IonCol>{item._competitionNom}</IonCol>
+              <IonCol>{item._buts}</IonCol>
+              <IonCol>{item._tirsPm}</IonCol>
+              <IonCol>{item._cartonJaune} {item._cartonRouge}</IonCol>
+              <IonCol>{item._possession}</IonCol>
+              <IonCol>{item._passesReussies}</IonCol>
+              <IonCol>{item._aeriensGagnes}</IonCol>
+              <IonCol>{item._note}</IonCol>
+            </IonRow>
+          ))}
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
