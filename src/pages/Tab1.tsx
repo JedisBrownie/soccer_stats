3c385915039b303034a378b7cb0bff8fc49a9dd6
JedisBrownie <Natana.ralambomanana@gmail.com>
2023-12-02 19:24:03 +0300

    Update Tab1.tsx

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -17,34 +17,22 @@ import './Tab1.css';
 
 const Tab1: React.FC = () => {
   const [selectedOption, setSelectedOption] = useState<string>('General');
+  const [url, setUrl] = useState<string>('https://soccer.bsite.net/soccer-stats-controller/general/2');
   const [data, setData] = useState<any[]>([]);
 
-  var url = '';
-
-  switch (selectedOption) {
-    case 'General':
-      url = 'https://soccer.bsite.net/soccer-stats-controller/general/2';
-      break;
-    case 'Domicile':
-      url = 'https://soccer.bsite.net/soccer-stats-controller/general/0';
-      break;
-    case 'Exterieur':
-      url = 'https://soccer.bsite.net/soccer-stats-controller/general/1';
-      break;
-  }
+  const fetchData = async (fetchUrl: string) => {
+    try {
+      const response = await axios.get(fetchUrl);
+      console.log(response.data);
+      setData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(url);
-        console.log(response.data);
-        setData(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchData();
-  }, []);
+    fetchData(url);
+  }, [url]);
 
   const renderGrid = () => {
     switch (selectedOption) {
@@ -52,97 +40,112 @@ const Tab1: React.FC = () => {
         return (
           <IonGrid>
             <IonRow className='theme'>
-
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Equipe</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Compétition</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Buts</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Tirs pm</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Discipline</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Possession%</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>PasseRéussies%</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>AériensGagnés</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Note</IonCol>
-
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Equipe</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Compétition</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Buts</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Tirs pm</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Discipline</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Possession%</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>PasseRéussies%</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>AériensGagnés</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Note</IonCol>
             </IonRow>
-            <IonRow className='grid_content'>
 
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Bayern Munich</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Bundesliga</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>43</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>7.7</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>13.3</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>7.7</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>9.7</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>1.7</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>7.21</IonCol>
-
+            {data.map((item, index) => (
+            <IonRow className='grid_content' key={index}>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._equipeNom}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._competitionNom}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._buts}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._tirsPm}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._cartonJaune} {item._cartonRouge}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._possession}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._passesReussies}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._aeriensGagnes}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._note}</IonCol>
             </IonRow>
+            ))}
           </IonGrid>
         );
       case 'Domicile':
         return (
           <IonGrid>
             <IonRow className='theme'>
-
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Equipe</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Compétition</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Buts</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Tirs pm</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Discipline</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Possession%</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>PasseRéussies%</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>AériensGagnés</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Note</IonCol>
-
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Equipe</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Compétition</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Buts</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Tirs pm</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Discipline</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Possession%</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>PasseRéussies%</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>AériensGagnés</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Note</IonCol>
             </IonRow>
-            <IonRow className='grid_content'>
 
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Bayern Munich</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Bundesliga</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>43</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>7.7</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>13.3</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>7.7</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>9.7</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>1.7</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>7.21</IonCol>
-
+            {data.map((item, index) => (
+            <IonRow className='grid_content' key={index}>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._equipeNom}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._competitionNom}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._buts}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._tirsPm}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._cartonJaune} {item._cartonRouge}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._possession}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._passesReussies}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._aeriensGagnes}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._note}</IonCol>
             </IonRow>
+            ))}
           </IonGrid>
         );
       case 'Exterieur':
         return (
           <IonGrid>
             <IonRow className='theme'>
-
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Equipe</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Compétition</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Buts</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Tirs pm</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Discipline</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Possession%</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>PasseRéussies%</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>AériensGagnés</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Note</IonCol>
-
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Equipe</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Compétition</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Buts</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Tirs pm</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Discipline</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Possession%</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>PasseRéussies%</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>AériensGagnés</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Note</IonCol>
             </IonRow>
-            <IonRow className='grid_content'>
 
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Bayern Munich</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>Bundesliga</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>43</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>7.7</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>13.3</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>7.7</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>9.7</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>1.7</IonCol>
-            <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>7.21</IonCol>
-
+            {data.map((item, index) => (
+            <IonRow className='grid_content' key={index}>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._equipeNom}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._competitionNom}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._buts}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._tirsPm}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._cartonJaune} {item._cartonRouge}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._possession}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._passesReussies}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._aeriensGagnes}</IonCol>
+              <IonCol size='1.333' sizeMd='1.333' sizeLg='1.333' sizeSm='1.333'>{item._note}</IonCol>
             </IonRow>
+            ))}
           </IonGrid>
         );
       default:
         return null;
+    }
+  };
+
+  const handleSelectChange = (value: string) => {
+    setSelectedOption(value);
+
+    switch (value) {
+      case 'General':
+        setUrl('https://soccer.bsite.net/soccer-stats-controller/general/2');
+        break;
+      case 'Domicile':
+        setUrl('https://soccer.bsite.net/soccer-stats-controller/general/0');
+        break;
+      case 'Exterieur':
+        setUrl('https://soccer.bsite.net/soccer-stats-controller/general/1');
+        break;
+      default:
+        break;
     }
   };
 
@@ -168,7 +171,7 @@ const Tab1: React.FC = () => {
                 label="Categorie des statistique recherchée"
                 labelPlacement="floating"
                 value={selectedOption}
-                onIonChange={(e) => setSelectedOption(e.detail.value)}
+                onIonChange={(e) => handleSelectChange(e.detail.value)}
               >
                 <IonSelectOption value="General">General</IonSelectOption>
                 <IonSelectOption value="Domicile">Domicile</IonSelectOption>
